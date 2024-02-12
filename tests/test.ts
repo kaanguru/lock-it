58ddc9dc06d691c0b3171b1ddcac40ab9c781b94
kaanguru <kaanguru@mail.ru>
2024-02-13 00:17:45 +0300

    update 3 files and create 1 file

--- tests/test.ts
+++ tests/test.ts
@@ -1,14 +1,21 @@
 import { expect, test } from '@playwright/test';
-
-test('add computer with  name', async ({ page }) => {
+import { generateMock } from '@anatine/zod-mock';
+import { _computerSchema } from "../src/lib/db";
+import { randomInt } from 'crypto';
+const seed = randomInt(3,99);
+const mockData = generateMock(_computerSchema,{seed});
+test('add computer with just name', async ({ page }) => {
+	test.slow();
+	console.log(mockData);
 	await page.goto('/');
-	await page.getByTitle('Name').fill('of02');
+	await page.getByTitle('Name').fill(mockData.name);
 	await page.getByRole('button').click();
-	await expect(page.getByRole('paragraph').getByText('of02')).toBeVisible();
+	await expect(page.getByRole('paragraph').getByText(mockData.name)).toBeVisible();
 });
 test('add computer with correct name and wrong ip address', async ({ page }) => {
+	test.slow();
 	await page.goto('/');
-	await page.getByTitle('Name').fill('of02');
+	await page.getByTitle('Name').fill(mockData.name);
 	await page.getByTitle('IPAddress').fill('22');
 	await page.getByRole('button').click();
 	await expect(page.getByRole('paragraph').getByText('Form is invalid!')).toBeVisible();
