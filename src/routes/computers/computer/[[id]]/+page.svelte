4ae2c455b4295593c3b119654c0fe9e5a6acd094
kaanguru <kaanguru@mail.ru>
2024-02-18 20:06:24 +0300

    update +page.svelte

--- src/routes/computers/computer/[[id]]/+page.svelte
+++ src/routes/computers/computer/[[id]]/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	export let data;
+	type KeyType = keyof typeof data.selectedComputer;
 </script>
 
 <div class="flex max-w-md justify-center">
-	<h1>{data.selectedComputer?.name}</h1>
-	{#if data.selectedComputer?.ipAddress}
-		<p>Mac Address: {data.selectedComputer?.macAddress}</p>
-	{/if}
+	<div class="card">
+		<header class="card-header"><h1>{data.selectedComputer?.name}</h1></header>
+		<section class="p-4">
+			{#each Object.keys(data.selectedComputer) as key}
+				{#if data.selectedComputer}
+				<p>{key}: {data.selectedComputer[key]}</p>			{/each}
+			<!-- {#if data.selectedComputer?.ipAddress}
+				<p>IP Address: {data.selectedComputer?.ipAddress}</p>
+			{/if}
+			{#if data.selectedComputer?.macAddress}
+				<p>IP Address: {data.selectedComputer?.macAddress}</p>
+			{/if} -->
+		</section>
+		<footer class="card-footer">(footer)</footer>
+	</div>
 </div>
