8773845fafa44120ef327ad4a497f25ea39a12a0
kaanguru <kaanguru@mail.ru>
2024-02-18 06:49:24 +0300

    update AddComputerForm.svelte and +page.svelte

--- src/routes/computers/+page.svelte
+++ src/routes/computers/+page.svelte
@@ -2,7 +2,6 @@
 	import { _computerSchema } from '$lib/db';
 	import { goto } from '$app/navigation';
 	let { data } = $props();
-
 </script>
 
 <div class="mx-auto max-w-md flex basis-2 flex-col">
@@ -14,21 +13,22 @@
 	{#if !data.computers}
 		<p>No computers found.</p>
 	{:else}
-	
-<nav class="list-nav">
-	<!-- (optionally you can provide a label here) -->
-	<ul>
-		{#each data.computers  as computer}
+		<nav class="list-nav">
+			<label for="ul">Computers</label>
+			<ul>
+				{#each data.computers as computer}
 					<li>
-			<a href="/computers/computer/{computer.id}">
-				<span class="badge bg-primary-500">({computer.id})</span>
-				<span class="flex-auto">{computer.name}</span>
-			</a>
-		</li>
-		{/each}
-
-	</ul>
-</nav>
-
+						<a href="/computers/computer/{computer.id}">
+							<span class="badge bg-primary-500">🖥️</span>
+							<span class="flex-auto">
+								<dt>{computer.name}</dt>
+								<dd>{computer.removeConnectionSoftware}</dd>
+								<dd>{computer.ipAddress}</dd>
+							</span>
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</nav>
 	{/if}
 </div>
