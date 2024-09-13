d66384401c7eee003fefaf45652efeffd15769b3
kaanguru <kaanguru@mail.ru>
2024-09-13 14:25:45 +0300

    update +page.svelte

--- app/src/routes/computers/add/+page.svelte
+++ app/src/routes/computers/add/+page.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import AddComputerForm from '$lib/components/AddComputerForm.svelte';
+	import { loggedIn } from '$lib/store';
+
+	loggedIn.subscribe((v) => {
+		if (v) {
+			console.log('🟩');
+		} else {
+			console.log('🟥');
+			goto('/');
+		}
+	});
 </script>
 
 <div class="flex justify-center">
